b3abcd2b1f4b977df8614445b6912580f2d5a125
gekas145 <56126336+gekas145@users.noreply.github.com>
2022-09-18 23:50:56 +0200

    add evaluation

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -1,8 +1,10 @@
 import sys
 import numpy as np
-
+import itertools as it
 
 def main():
+
+    bytes = ((0, 0), (0, 1), (1, 0), (1, 1))
 
     def fill_plane():
         missing = [[x for x in range(1, 10) if x not in sudoku_plane[i]] for i in range(9)]
@@ -13,8 +15,19 @@ def main():
             missing_idxs = np.where(sudoku_plane[i] == 0)[0]
             sudoku_plane[i][missing_idxs] = missing[i]
 
+    def count_unique(arr):
+        return len(np.unique(arr))
+    
+    def evaluate(plane):
+        score = 162 # 9**2 + 9**2
+        score -= np.sum([count_unique(plane[3*b[0]:3*b[0]+3,
+                                            3*b[1]:3*b[1]+3]) for b in bytes])
+        
+        score -= np.sum([count_unique(plane[:,j]) for j in range(9)])
+        
+        return score
 
-
+    
     if len(sys.argv) == 1:
         sudoku_file = 'sudoku.txt'
     else:
@@ -27,14 +40,12 @@ def main():
     sudoku_plane = [[int(line[i]) for i in range(9)] for line in sudoku_plane]
 
     sudoku_plane = np.array(sudoku_plane, dtype=np.int8)
-    print(sudoku_plane[0])
 
     fill_plane()
 
-    print(sudoku_plane[0])
+    print(evaluate(sudoku_plane))
 
-
-
+    print(sudoku_plane)
 
 
 
